46d8c8b24d939f216fb88b13b6921755e20fd86b
jjorgm <jordangarciamoquete@gmail.com>
2026-07-20 22:01:44 -0400

    limit to 60 FPS

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from logger import log_event, log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -13,7 +15,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
             screen.fill("black")
-
+        dt += clock.tick(60) / 1000.0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
