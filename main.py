8a123627479af0e9d16f6cc2f0e863e3e9cf0c71
jjorgm <jordangarciamoquete@gmail.com>
2026-07-21 16:10:07 -0400

    created updateable and drawable player containers, fixed rotation and movement speed issue

--- main.py
+++ main.py
@@ -10,16 +10,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen.fill("black")
-        dt += clock.tick(60) / 1000.0
-        player.update(dt)
-        player.draw(screen)
+
+        dt = clock.tick(60) / 1000.0
+        updatable.update(dt)
+        screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
